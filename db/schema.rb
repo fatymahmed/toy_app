7d9ac684babd311d29794261ba5d23fa3f03076a
fatima ahmed <fatymah001@gmail.com>
2019-08-22 19:32:55 +0300

    add user/index as root

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_821_224_532) do
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema.define(version: 20190821224532) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
